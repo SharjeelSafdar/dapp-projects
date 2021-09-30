4ce6bd61f87d18d215613ca6c3c6c7b63b5da030
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-09-30 19:50:25 +0500

    Updated shares token name in migration script

--- p6-erc20-payment-splitter/smartContracts/migrations/1_deploy_contracts.js
+++ p6-erc20-payment-splitter/smartContracts/migrations/1_deploy_contracts.js
@@ -1,14 +1,14 @@
 const FakeDai = artifacts.require("FakeDai");
-const ApiToken = artifacts.require("ApiToken");
+const SharesToken = artifacts.require("SharesToken");
 const MySplitter = artifacts.require("MySplitter");
 
 module.exports = async function (deployer, _, accounts) {
-  console.log({ accounts });
+  // console.log({ accounts });
 
-  await deployer.deploy(FakeDai, { from: accounts[5] });
-  await deployer.deploy(ApiToken, "ApiToken", "ATKN", accounts.slice(0, 5));
+  await deployer.deploy(FakeDai);
+  await deployer.deploy(SharesToken);
 
   const fakeDai = await FakeDai.deployed();
-  const apiToken = await ApiToken.deployed();
-  await deployer.deploy(MySplitter, apiToken.address, fakeDai.address);
+  const sharesToken = await SharesToken.deployed();
+  await deployer.deploy(MySplitter, sharesToken.address, fakeDai.address);
 };
