8807033bb4c03dcf0871c15b4ea3ab579888d5fc
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-10-01 02:36:51 +0500

    Added the functionality in fake DAI to store its holders, and redployed

--- p6-erc20-payment-splitter/smartContracts/contracts/SharesToken.sol
+++ p6-erc20-payment-splitter/smartContracts/contracts/SharesToken.sol
@@ -7,12 +7,28 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "./ERC20Shares.sol";
 
 contract SharesToken is Context, ERC20, ERC20Permit, ERC20Shares {
+  address[] private _holders;
+
   constructor() ERC20("SharesToken", "SHA") ERC20Permit("SharesToken") {}
 
   function sendMe500Shares() external {
     _mint(_msgSender(), 500 * 10 ** decimals());
   }
 
+  function getHolders() public view returns (address[] memory) {
+    return _holders;
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 amount)
+    internal override
+  {
+    super._beforeTokenTransfer(from, to, amount);
+
+    if (balanceOf(to) == 0) {
+      _holders.push(to);
+    }
+  }
+ 
   function _afterTokenTransfer(address from, address to, uint256 amount)
     internal override(ERC20, ERC20Shares)
   {
