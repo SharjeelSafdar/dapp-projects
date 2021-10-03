24f2c43e1f9983f8ebd81f8d89b7b5ac82a30c15
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-10-03 18:59:08 +0500

    Updated migrations script according to the new contracts

--- p6-erc20-payment-splitter/smartContracts/migrations/1_deploy_contracts.js
+++ p6-erc20-payment-splitter/smartContracts/migrations/1_deploy_contracts.js
@@ -11,4 +11,13 @@ module.exports = async function (deployer, _, accounts) {
   const fakeDai = await FakeDai.deployed();
   const sharesToken = await SharesToken.deployed();
   await deployer.deploy(MySplitter, sharesToken.address, fakeDai.address);
+
+  // Grant SNAPSHOT_CREATOR role to MySplitter.
+  const mySplitter = await MySplitter.deployed();
+  const snapshotCreatorRole = await sharesToken.SNAPSHOT_CREATOR();
+  await sharesToken.grantRole(snapshotCreatorRole, mySplitter.address);
+
+  // Revoke ERC20_SHARES_ADMIN_ROLE role from contract deployer.
+  const erc20SharesAdminRole = await sharesToken.ERC20_SHARES_ADMIN_ROLE();
+  await sharesToken.renounceRole(erc20SharesAdminRole, accounts[0]);
 };
